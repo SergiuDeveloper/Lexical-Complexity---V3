aced1526a96d36b4a858d675a344a92a1159b3e5
SergiuDev <sergiu.nistor@info.uaic.ro>
2021-04-13 15:11:45 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+from gensim.models.fasttext import FastText
+from joblib import load
+import sys
+
+
+fasttext_mdl = FastText.load('models/FastText/model')
+hgbr_mdl = load('models/HistGradientBoostingRegressor/model')
+
+input_word = sys.argv[1].lower()
+input_word_ngrams = fasttext_mdl.wv[input_word]
+predicted_complexity = hgbr_mdl.predict([input_word_ngrams])[0]
+print(predicted_complexity)
